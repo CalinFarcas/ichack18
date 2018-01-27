01009c23d1552febf24717dd0a4a5b5282c326ed
Bianca-Alexandra Casapu <bianca-alexandra.casapu17@imperial.ac.uk>
2018-01-27 14:09:01 +0000

    implemented coords class, started image analyzer

---
+++ src/picture/Coords.java
@@ -0,0 +1,34 @@
+package picture;
+
+public class Coords {
+
+  private int x;
+  private int y;
+
+  public Coords(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  @Override
+  public boolean equals(Coords coords) {
+    return this.x == coords.getX() && this.y == coords.getY();
+  }
+  
+}
---
+++ src/picture/ImageAnalyzer.java
@@ -0,0 +1,22 @@
+package picture;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ImageAnalyzer {
+
+  //bfs odata
+  public Zone createZone(Color startPixel, Zone zone) {
+    //zone.pixelList.add(startPixel);
+    Deque<Color> queue = new ArrayDeque<>();
+    queue.add(startPixel);
+
+    while (queue.size() > 0) {
+
+    }
+
+
+  }
+
+  //bfs pe toata imaginea
+}
